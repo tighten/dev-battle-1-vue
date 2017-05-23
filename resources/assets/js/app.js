79c989c1c04a54f9679b87f01ba4496c57b568c3
Keith Damiani <keith.damiani@gmail.com>
2017-05-23 10:35:13 -0400

    Add central data store to app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,8 +1,38 @@
 require('./bootstrap');
 
 import router from './routes';
+import ApiClient from './api/api-client.js';
 
-const app = new Vue({
+window.store = new Vue({
+    mixins: [ ApiClient ],
+
+    data () {
+        return {
+            tweets: [],
+            username: clash.username,
+        }
+    },
+
+    methods: {
+        add (resource, data) {
+            this[resource].push(data);
+        },
+        delete(resource, element) {
+            var element_id = this[resource].indexOf(element);
+
+            if (element_id > -1) {
+                this[resource].splice(element_id, 1);
+            }
+        },
+        load(resource, endpoint) {
+            return this.get(endpoint).then((response) => {
+                this[resource] = response;
+            });
+        },
+    }
+});
+
+window.app = new Vue({
     el: '#app',
 
     router,
