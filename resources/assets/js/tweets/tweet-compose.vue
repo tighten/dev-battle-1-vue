a7e4c392a887e3a03f819e9a03bbc4cb15fcb4d4
Keith Damiani <keith.damiani@gmail.com>
2017-05-23 10:53:13 -0400

    Add red text to tweet-compose, refactor, update styling, add transition

--- resources/assets/js/tweets/tweet-compose.vue
+++ resources/assets/js/tweets/tweet-compose.vue
@@ -1,65 +1,116 @@
 <template>
-    <div>
-        <button class="btn btn-primary" @click="compose">
-            Compose Tweet
+    <div class="compose">
+        <button
+            :class="['btn', 'btn-block', 'btn-blue', { 'btn-blue--active': composing }]"
+            @click="compose"
+        >
+            <i class="icon ion-md-create"></i>Compose Tweet
         </button>
 
-        <div v-if="compose_tweet">
-            <input-text
-                name="compose"
-                v-model="text"
-            >
-            </input-text>
+        <transition name="compose-slide-down">
+            <div v-show="composing" class="compose__container">
+                <div class="compose__body">
+                    <tweet-avatar></tweet-avatar>
 
-            {{ character_count }}
+                    <div class="compose__editor"
+                        contenteditable
+                        ref="textarea"
+                        @input="updateBody($event.target.innerHTML)"
+                        @keypress.enter.prevent
+                    ></div>
 
-            <button class="btn btn-primary" :disabled="character_count < 0" @click="submit">
-                Tweet!
-            </button>
-        </div>
+                    <div class="compose__editor-highlighted"
+                        v-html="body_formatted"
+                    ></div>
+                </div>
+
+                <div class="compose__footer">
+                    <span :class="['compose__counter', characters_remaining_class]">
+                        {{ characters_remaining }}
+                    </span>
+
+                    <button
+                        class="btn btn-blue"
+                        :disabled="can_submit"
+                        @click="submit"
+                    >
+                        <i class="icon ion-logo-twitter"></i>Tweet
+                    </button>
+                </div>
+            </div>
+        </transition>
     </div>
 </template>
 
 <script>
+const maximum_characters = 140;
+
 import ApiClient from '../api/api-client.js';
-import InputText from '../mixins/form/input-text.vue';
+import TweetAvatar from './tweet-avatar.vue';
 
 export default {
     mixins: [ ApiClient ],
 
-    components: {
-        InputText,
-    },
-
-    computed: {
-        character_count () {
-            return 140 - this.text.length;
-        }
-    },
+    components: { TweetAvatar },
 
     data() {
         return {
-            response: '',
-            author: 'Keith Damiani',
-            text: '',
-            compose_tweet: false,
+            body: '',
+            composing: false,
         }
+    },
+
+    computed: {
+        body_formatted() {
+            return this.body.substring(0, maximum_characters) + '<span class="highlighted">' + this.body.substring(maximum_characters) + '</span>';
+        },
+        can_submit() {
+            return this.characters_remaining == maximum_characters
+                || this.characters_remaining < 0;
+        },
+        characters_remaining() { return maximum_characters - this.body.length },
+        characters_remaining_class() {
+            if (this.characters_remaining <= 10) {
+                return 'compose__counter--danger';
+            } else if (this.characters_remaining <= 20) {
+                return 'compose__counter--warning';
+            }
+        },
     },
 
     methods: {
         compose() {
-            this.compose_tweet = true;
+            this.composing = ! this.composing;
+        },
+        reset() {
+            this.body = '';
+            this.composing = false;
+            this.$refs.textarea.innerHTML = '';
         },
         submit() {
             let payload = {
-                author: this.author,
-                text: this.text ? this.text : 'Blank tweets were not in the spec, @stauffermatt',
+                author: store.username,
+                text: this.body,
             }
 
             this.post('tweets', payload).then((response) => {
-                this.$emit('added', response);
+                store.add('tweets', response);
+                this.reset();
             });
         },
+        updateBody(text) {
+            this.body = text;
+        },
     },
+
+    watch: {
+        composing(val) {
+            if (val) {
+                this.$nextTick(() => {
+                    this.$refs.textarea.focus();
+                });
+            }
+        },
+    }
 }
 </script>
