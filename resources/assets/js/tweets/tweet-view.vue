9d13aebf34601ee70fa6d38cdaed8c1afd15c180
Keith Damiani <keith.damiani@gmail.com>
2017-05-23 10:44:11 -0400

    Improve styling of tweet-view

--- resources/assets/js/tweets/tweet-view.vue
+++ resources/assets/js/tweets/tweet-view.vue
@@ -1,19 +1,25 @@
 <template>
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <p>{{ tweet.author }}</p>
-            <p>{{ tweet.text }}</p>
-            <p>{{ tweet.created_at }}</p>
+    <div class="tweet">
+        <div class="tweet__content">
+            <div class="tweet__details">
+                <p><strong>{{ tweet.author }}</strong></p>
+
+                <p class="text-muted">{{ tweet.created_at | human_date }}</p>
+            </div>
+
+            <h3 class="tweet__text">{{ tweet.text }}</h3>
         </div>
     </div>
 </template>
 
 <script>
-import ApiClient from '../api/api-client.js';
+import moment from 'moment';
 
 export default {
-    mixins: [ ApiClient ],
-
     props: [ 'tweet' ],
+
+    filters: {
+        human_date(date) { return moment.utc(date, 'YYYY-MM-DD hh:mm:ss').fromNow() },
+    },
 }
 </script>
